b52cd658fba4d1a58b3c4c0f7567a679aa323621
Ryan Atkinson <mail@ryanatkn.com>
2023-03-14 23:52:38 -0600

    fixes

---
+++ src/lib/music/InitMidiButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type MidiInput from '$lib/audio/MidiInput.svelte';
+
+	export let midi_input: MidiInput | undefined;
+
+	$: midi_access = midi_input?.midi_access;
+</script>
+
+<button
+	class="big w-full"
+	on:click={() => void midi_input?.init()}
+	disabled={!midi_input || !!$midi_access}
+	title={midi_input ? ($midi_access ? 'MIDI is ready!' : 'connect your MIDI device') : 'loading...'}
+>
+	init MIDI
+</button>
