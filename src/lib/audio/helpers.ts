63923c1da209a9e78381e0bb408b191057d4ce91
Ryan Atkinson <mail@ryanatkn.com>
2023-03-17 00:36:44 -0600

    gp

--- src/lib/audio/helpers.ts
+++ src/lib/audio/helpers.ts
@@ -1,7 +1,7 @@
 /**
  * Convert a user-facing volume value [0,1] to the actual gain value.
  * We want some sort of nonlinear curve to match user expectations.
- * TODO why is this exponent so different from the article?
+ * TODO why is this exponent so different from the article? this sounds better to me
  * https://www.dr-lex.be/info-stuff/volumecontrols.html
  */
 export const VOLUME_TO_GAIN_EXPONENT = 2.2;
