159fa508bfd30ba0423fb8696d429880c454f586
Ryan Atkinson <mail@ryanatkn.com>
2023-03-21 09:31:22 -0600

    flow

--- src/lib/earbetter/LevelDefForm.svelte
+++ src/lib/earbetter/LevelDefForm.svelte
@@ -64,6 +64,9 @@
 		note_max !== level_def.note_max ||
 		intervals.toString() !== level_def.intervals.toString();
 
+	let expanded = false;
+	$: if (editing) expanded = true;
+
 	const import_data = () => {
 		const data = to_data();
 		const serialized = JSON.stringify(data);
@@ -85,103 +88,109 @@
 			{#if editing}editing level{:else}create a custom level{/if}
 		</h2>
 	</header>
-	<fieldset>
-		<label>
-			<div class="title">name</div>
-			<input bind:value={name} />
-		</label>
-	</fieldset>
-	<fieldset>
-		<label>
-			<div class="title">intervals</div>
-			<input
-				value={serialize_intervals(intervals)}
-				on:input={(e) => (intervals = parse_intervals(e.currentTarget.value))}
-			/>
-		</label>
-		<details>
-			<summary>more info</summary>
-			<p>
-				this is a comma-separated list of numbers representing the
-				<a href="https://wikipedia.org/wiki/Interval_(music)">musical intervals</a> used in the
-				level, aka the number of piano keys (<a href="https://wikipedia.org/wiki/Semitone"
-					>semitones</a
-				>) separating each note from the
-				<a href="https://wikipedia.org/wiki/Tonic_(music)">tonic</a> (the base note)
-			</p>
-			<p>
-				for example, to practice distinguishing between the <a
-					href="https://wikipedia.org/wiki/Perfect_fourth">perfect fourth</a
-				>
-				and <a href="https://wikipedia.org/wiki/Perfect_fifth">perfect fifth</a>, enter "<code
-					>5, 7</code
-				>"
-			</p>
-			<p>negative numbers are allowed</p>
-		</details>
-	</fieldset>
-	<fieldset>
-		<label>
-			<div class="title">trial_count</div>
-			<input type="number" bind:value={trial_count} min={1} />
-			<input type="range" bind:value={trial_count} min={1} max={20} />
-		</label>
-	</fieldset>
-	<fieldset>
-		<label>
-			<div class="title">sequence_length</div>
-			<input bind:value={sequence_length} min={2} />
-			<input type="range" bind:value={sequence_length} min={2} max={16} />
-		</label>
-	</fieldset>
-	<fieldset class="row">
-		<label>
-			<div class="title">MIDI min</div>
-			<div>{midi_names[note_min]}</div>
-			<input type="number" bind:value={note_min} step={1} min={MIDI_MIN} max={MIDI_MAX} />
-			<input type="range" bind:value={note_min} step={1} min={MIDI_MIN} max={MIDI_MAX} />
-		</label>
-		<label>
-			<div class="title">MIDI max</div>
-			<div>{midi_names[note_max]}</div>
-			<input type="number" bind:value={note_max} step={1} min={MIDI_MIN} max={MIDI_MAX} />
-			<input type="range" bind:value={note_max} step={1} min={MIDI_MIN} max={MIDI_MAX} />
-		</label>
-	</fieldset>
-	<fieldset>
-		<label>
-			<div class="title">id</div>
-			<input disabled value={id} />
-		</label>
-	</fieldset>
-	<button
-		type="button"
-		on:click={() => dispatch('submit', to_data())}
-		disabled={editing && !changed}
-	>
-		{#if editing}save changes to level{:else}create level{/if}
-	</button>
-	<button type="button" on:click={import_data}>
-		{#if editing}import/export data{:else}import data{/if}
-	</button>
-	{#if editing}
-		<button type="button" on:click={() => (removing = !removing)}> remove level </button>
-		{#if removing}
-			<div transition:slide|local>
-				<button
-					class="w-full"
-					type="button"
-					on:click={() => {
-						removing = false;
-						dispatch('remove', id);
-					}}
-				>
-					✖ confirm remove
-				</button>
-			</div>
-		{/if}
+	{#if editing || expanded}
+		<div class="fields" in:slide|local>
+			<fieldset>
+				<label>
+					<div class="title">name</div>
+					<input bind:value={name} />
+				</label>
+			</fieldset>
+			<fieldset>
+				<label>
+					<div class="title">intervals</div>
+					<input
+						value={serialize_intervals(intervals)}
+						on:input={(e) => (intervals = parse_intervals(e.currentTarget.value))}
+					/>
+				</label>
+				<details>
+					<summary>more info</summary>
+					<p>
+						this is a comma-separated list of numbers representing the
+						<a href="https://wikipedia.org/wiki/Interval_(music)">musical intervals</a> used in the
+						level, aka the number of piano keys (<a href="https://wikipedia.org/wiki/Semitone"
+							>semitones</a
+						>) separating each note from the
+						<a href="https://wikipedia.org/wiki/Tonic_(music)">tonic</a> (the base note)
+					</p>
+					<p>
+						for example, to practice distinguishing between the <a
+							href="https://wikipedia.org/wiki/Perfect_fourth">perfect fourth</a
+						>
+						and <a href="https://wikipedia.org/wiki/Perfect_fifth">perfect fifth</a>, enter "<code
+							>5, 7</code
+						>"
+					</p>
+					<p>negative numbers are allowed</p>
+				</details>
+			</fieldset>
+			<fieldset>
+				<label>
+					<div class="title">trial_count</div>
+					<input type="number" bind:value={trial_count} min={1} />
+					<input type="range" bind:value={trial_count} min={1} max={20} />
+				</label>
+			</fieldset>
+			<fieldset>
+				<label>
+					<div class="title">sequence_length</div>
+					<input bind:value={sequence_length} min={2} />
+					<input type="range" bind:value={sequence_length} min={2} max={16} />
+				</label>
+			</fieldset>
+			<fieldset class="row">
+				<label>
+					<div class="title">MIDI min</div>
+					<div>{midi_names[note_min]}</div>
+					<input type="number" bind:value={note_min} step={1} min={MIDI_MIN} max={MIDI_MAX} />
+					<input type="range" bind:value={note_min} step={1} min={MIDI_MIN} max={MIDI_MAX} />
+				</label>
+				<label>
+					<div class="title">MIDI max</div>
+					<div>{midi_names[note_max]}</div>
+					<input type="number" bind:value={note_max} step={1} min={MIDI_MIN} max={MIDI_MAX} />
+					<input type="range" bind:value={note_max} step={1} min={MIDI_MIN} max={MIDI_MAX} />
+				</label>
+			</fieldset>
+			<fieldset>
+				<label>
+					<div class="title">id</div>
+					<input disabled value={id} />
+				</label>
+			</fieldset>
+			<button
+				type="button"
+				on:click={() => dispatch('submit', to_data())}
+				disabled={editing && !changed}
+			>
+				{#if editing}save changes to level{:else}create level{/if}
+			</button>
+			<button type="button" on:click={import_data}>
+				{#if editing}import/export data{:else}import data{/if}
+			</button>
+			{#if editing}
+				<button type="button" on:click={() => (removing = !removing)}> remove level </button>
+				{#if removing}
+					<div transition:slide|local>
+						<button
+							class="w-full"
+							type="button"
+							on:click={() => {
+								removing = false;
+								dispatch('remove', id);
+							}}
+						>
+							✖ confirm remove
+						</button>
+					</div>
+				{/if}
+			{/if}
+			<slot name="footer" {changed} />
+		</div>
+	{:else}
+		<button on:click={() => (expanded = true)}>make a new level</button>
 	{/if}
-	<slot name="footer" {changed} />
 </form>
 
 <style>
@@ -189,5 +198,9 @@
 		font-size: var(--font_size_xl);
 		text-align: center;
 		margin-bottom: var(--spacing_md);
+	}
+	.fields {
+		display: flex;
+		flex-direction: column;
 	}
 </style>
