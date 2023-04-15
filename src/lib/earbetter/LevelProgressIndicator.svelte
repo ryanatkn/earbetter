8bbe902481d2e975cec6c1bc828e421d764390f6
Ryan Atkinson <mail@ryanatkn.com>
2023-04-15 09:45:01 -0600

    progress

--- src/lib/earbetter/LevelProgressIndicator.svelte
+++ src/lib/earbetter/LevelProgressIndicator.svelte
@@ -20,7 +20,7 @@
 		$status === 'complete' ? 1 : $trial ? ($trial.index + 0.5) / $def.trial_count : 0;
 </script>
 
-<div class="level-progress-indicator" style:--percent_complete={percent_complete}>
+<div class="level-progress-indicator" style:--progress_bar_percent={percent_complete}>
 	{#each {length: $def.trial_count} as _, index}
 		<div class="level" style="background-color: {get_bg_color($status, $trial, $trials, index)}" />
 	{/each}
@@ -38,15 +38,5 @@
 		border: var(--border_width) var(--border_style) var(--border_color);
 		background-color: transparent;
 		transition: background-color linear var(--duration_5);
-	}
-	.progress-bar {
-		position: absolute;
-		left: 0;
-		top: calc(50% - var(--spacing_md) / 2);
-		width: calc(var(--percent_complete) * 100%);
-		height: var(--spacing_md);
-		background-color: var(--tint_light_4);
-		border-radius: var(--border_radius);
-		transition: width var(--duration_4) ease;
 	}
 </style>
