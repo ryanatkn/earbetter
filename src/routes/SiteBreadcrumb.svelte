0585d5577ecc3aafa29e8c6d41b359fac82934c1
Ryan Atkinson <mail@ryanatkn.com>
2023-09-30 10:04:20 -0600

    wip

---
+++ src/routes/SiteBreadcrumb.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Breadcrumb from '@fuz.dev/fuz_library/Breadcrumb.svelte';
+	import {page} from '$app/stores';
+	import {base} from '$app/paths';
+
+	$: pathname = $page.url.pathname;
+	$: home = pathname === base + '/';
+</script>
+
+<!-- TODO this awkwardly nests a link -->
+<Breadcrumb
+	>{#if home}<a href="{base}/map">🎶🦜</a>{:else}🎶🦜{/if}</Breadcrumb
+>
