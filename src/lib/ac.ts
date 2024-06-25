a22a3f66b79a867562b8ebc99d6a5a9eb69ff813
Ryan Atkinson <mail@ryanatkn.com>
2024-06-25 12:00:47 -0600

    wip

--- src/lib/ac.ts
+++ src/lib/ac.ts
@@ -1,4 +1,6 @@
+import {browser} from '$app/environment';
 import {setContext, getContext} from 'svelte';
+import {noop} from '@ryanatkn/belt/function.js';
 
 // There's an unfortunate overlap between "context" as in
 // svelte's `getContext`/`setContext`, and the browser's `AudioContext`.
@@ -21,12 +23,32 @@ export const set_ac = (): (() => AudioContext) => {
 	return get_ac;
 };
 
+// TODO BLOCK @multiple where's the best place for this? needs to be synchronous with a click or similar - maybe `onclickcapture` on the main layout, and set context at that point
+// await ac.resume();
 /**
  * This should be called during a user input action like a click,
  * or it needs `resume` called for some browsers.
  */
 export const create_ac = (): AudioContext => {
-	const w: any = window;
-	const ac = new (w.AudioContext || w.webkitAudioContext)();
+	if (!browser) return new Audio_Context_Stub() as any;
+	const ac = new AudioContext();
 	return ac;
+};
+
+// TODO maybe this isn't the best way to do this
+class Audio_Context_Stub {
+	baseLatency = 0;
+	outputLatency = 0;
+	resume = noop;
+	suspend = noop;
+	close = noop;
+	createMediaElementSource = noop_ssr;
+	createMediaStreamDestination = noop_ssr;
+	createMediaStreamSource = noop_ssr;
+	createMediaStreamTrackSource = noop_ssr;
+	getOutputTimestamp = noop_ssr;
+}
+
+const noop_ssr = () => {
+	throw new Error('Cannot call this method outside of the browser');
 };
