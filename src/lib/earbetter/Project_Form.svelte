e36db2c799995eb1e69e4bdcec9f9a85060d4294
Ryan Atkinson <mail@ryanatkn.com>
2024-07-04 15:03:38 -0600

    wip

--- src/lib/earbetter/Project_Form.svelte
+++ src/lib/earbetter/Project_Form.svelte
@@ -58,7 +58,7 @@
 	let project_data_el: HTMLTextAreaElement | undefined = $state();
 	let start_importing_el: HTMLButtonElement | undefined = $state();
 
-	const import_data = async (): Promise<void> => {
+	const import_data = (): void => {
 		parse_error_message = '';
 		try {
 			const json = JSON.parse(updated);
