ab13e3f63075fcd02c7d5a63a25c954103dd6938
Ryan Atkinson <mail@ryanatkn.com>
2024-07-06 07:49:19 -0600

    wip

--- src/lib/projects/default_project.ts
+++ src/lib/projects/default_project.ts
@@ -16,52 +16,52 @@ const data = (): Project_Data =>
 			to_default_scale_realm('chromatic'),
 			// TODO this will change, just including to see where to go next
 			{
-				name: 'major third vs perfect fourth',
+				name: 'major second vs perfect fifth (easier for learning the app)',
 				levels: [
 					{
 						name: 'one octave',
 						sequence_length: 2,
-						intervals: [4, 5],
+						intervals: [2, 7],
 					},
 					{
 						name: 'one octave (downward)',
 						sequence_length: 2,
-						intervals: [-8, -7],
+						intervals: [-10, -5],
 					},
 					{
 						name: 'two octaves',
 						sequence_length: 2,
-						intervals: [4, 5, -8, -7],
+						intervals: [2, 7, -10, -5],
 					},
 					{
 						name: 'two octaves (downward)',
 						sequence_length: 2,
-						intervals: [-8, -7, -20, -19],
+						intervals: [-10, -5, -22, -17],
 					},
 					{
 						name: 'four octaves',
 						sequence_length: 2,
-						intervals: [4, 5, -8, -7, 16, 17, -20, -19],
+						intervals: [2, 7, -10, -5, 14, 19, -22, -17],
 						min_note: DEFAULT_MIN_NOTE - 12,
 					},
 					{
 						name: 'two octaves (4 notes)',
-						intervals: [4, 5, -8, -7],
+						intervals: [2, 7, -10, -5],
 						sequence_length: 4,
 					},
 					{
 						name: 'four octaves (4 notes)',
-						intervals: [4, 5, -8, -7, 16, 17, -20, -19],
+						intervals: [2, 7, -10, -5, 14, 19, -22, -17],
 						sequence_length: 4,
 					},
 					{
 						name: 'two octaves (8 notes)',
-						intervals: [4, 5, -8, -7],
+						intervals: [2, 7, -10, -5],
 						sequence_length: 8,
 					},
 					{
 						name: 'four octaves (8 notes)',
-						intervals: [4, 5, -8, -7, 16, 17, -20, -19],
+						intervals: [2, 7, -10, -5, 14, 19, -22, -17],
 						sequence_length: 8,
 					},
 				],
