82fd6b5b075bc55ee75a784cdb3f82e109212a01
Ryan Atkinson <mail@ryanatkn.com>
2024-10-26 18:37:36 -0600

    import moss.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '@ryanatkn/moss/style.css';
 	import '@ryanatkn/moss/theme.css';
 	import '$lib/style.css';
+	import '$routes/moss.css';
 
 	import Themed from '@ryanatkn/fuz/Themed.svelte';
 	import {is_editable, swallow} from '@ryanatkn/belt/dom.js';
